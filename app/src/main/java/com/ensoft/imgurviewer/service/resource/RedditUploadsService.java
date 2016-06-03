55a82132daa6fbb9e03621f30a260b1f18f4afb1
Mart??n Lucas Golini <spartanj@gmail.com>
2016-06-03 01:43:06 -0300

    Added i.reddituploads.com support.

---
+++ app/src/main/java/com/ensoft/imgurviewer/service/resource/RedditUploadsService.java
@@ -0,0 +1,29 @@
+package com.ensoft.imgurviewer.service.resource;
+
+import android.net.Uri;
+
+import com.ensoft.imgurviewer.service.listener.PathResolverListener;
+
+public class RedditUploadsService extends ImageServiceSolver
+{
+	public static final String TAG = RedditUploadsService.class.getCanonicalName();
+	public static final String REDDITUPLOADS_DOMAIN = "i.reddituploads.com";
+
+	@Override
+	public void getPath( Uri uri, PathResolverListener pathResolverListener )
+	{
+		pathResolverListener.onPathResolved( uri, null );
+	}
+
+	@Override
+	public boolean isServicePath( Uri uri )
+	{
+		return uri.toString().contains( REDDITUPLOADS_DOMAIN );
+	}
+
+	@Override
+	public boolean isGallery( Uri uri )
+	{
+		return false;
+	}
+}
