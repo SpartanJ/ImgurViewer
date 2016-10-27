9acbad6e9f8903404e630b5ea15cabd3f159a041
Martín Lucas Golini <spartanj@gmail.com>
2016-10-27 02:08:02 -0300

    Improved the video format check from URI's ( this fixes streamable and i.reddit.com links ).

--- app/src/main/java/com/ensoft/imgurviewer/service/resource/ImageServiceSolver.java
+++ app/src/main/java/com/ensoft/imgurviewer/service/resource/ImageServiceSolver.java
@@ -4,16 +4,36 @@ import android.net.Uri;
 
 import com.ensoft.imgurviewer.service.listener.PathResolverListener;
 
+import java.util.List;
+
 public abstract class ImageServiceSolver
 {
 	public static boolean isVideoUrl( Uri uri )
 	{
-		return isVideoUrl( uri.toString() );
+		if ( null != uri )
+		{
+			List<String> pathSegments = uri.getPathSegments();
+
+			if ( null != pathSegments && pathSegments.size() > 0 )
+			{
+				String lastPathSegment = pathSegments.get( pathSegments.size() - 1 );
+
+				return lastPathSegment.endsWith( ".gifv" ) ||
+					lastPathSegment.endsWith( ".mp4" ) ||
+					lastPathSegment.endsWith( ".avi" ) ||
+					lastPathSegment.endsWith( ".flv" ) ||
+					lastPathSegment.endsWith( ".mkv" ) ||
+					lastPathSegment.endsWith( ".webm" ) ||
+					( lastPathSegment.endsWith( ".gif" ) && uri.toString().contains( "fm=mp4" ) );
+			}
+		}
+
+		return false;
 	}
 
 	public static boolean isVideoUrl( String uri )
 	{
-		return uri.endsWith( ".gifv" ) || uri.endsWith( ".mp4" ) || uri.endsWith( ".avi" ) || uri.endsWith( ".flv" ) || uri.endsWith( ".mkv" ) || uri.endsWith( ".webm" );
+		return isVideoUrl( Uri.parse( uri ) );
 	}
 
 	public abstract void getPath( Uri uri, final PathResolverListener pathResolverListener );
@@ -22,7 +42,7 @@ public abstract class ImageServiceSolver
 
 	public boolean isVideo( Uri uri )
 	{
-		return isVideo( uri.toString() );
+		return isVideoUrl( uri );
 	}
 
 	public boolean isVideo( String uri )
