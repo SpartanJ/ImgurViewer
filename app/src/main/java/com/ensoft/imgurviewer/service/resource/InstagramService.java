3bc09e15471b4037da5ead97649c4ad95ff0fbb7
Martín Lucas Golini <spartanj@gmail.com>
2016-12-28 15:21:52 -0300

    InstagramService now detects videos on instagram media links.

--- app/src/main/java/com/ensoft/imgurviewer/service/resource/InstagramService.java
+++ app/src/main/java/com/ensoft/imgurviewer/service/resource/InstagramService.java
@@ -1,10 +1,15 @@
 package com.ensoft.imgurviewer.service.resource;
 
 import android.net.Uri;
+import android.util.Log;
 
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
 import com.ensoft.imgurviewer.App;
 import com.ensoft.imgurviewer.service.StringUtils;
 import com.ensoft.imgurviewer.service.listener.PathResolverListener;
+import com.ensoft.restafari.network.service.RequestService;
 import com.imgurviewer.R;
 
 public class InstagramService extends ImageServiceSolver
@@ -39,19 +44,67 @@ public class InstagramService extends ImageServiceSolver
 		return null;
 	}
 
-	@Override
-	public void getPath( Uri uri, PathResolverListener pathResolverListener )
+	protected Uri getVideoUrlFromResponse( String response )
 	{
-		String mediaUrl = getMediaUrl( uri );
+		String videoSecureUrl = "og:video:secure_url\" content=\"";
 
-		if ( mediaUrl != null )
+		int pos = response.lastIndexOf( videoSecureUrl );
+
+		if ( -1 != pos )
 		{
-			pathResolverListener.onPathResolved( Uri.parse( mediaUrl + "?size=l" ), Uri.parse( mediaUrl + "?size=t" ) );
+			int endPos = response.indexOf( "\"", pos + videoSecureUrl.length() );
+
+			if ( -1 != endPos )
+			{
+				String videoUrl = response.substring( pos + videoSecureUrl.length(), endPos );
+
+				return Uri.parse( videoUrl );
+			}
 		}
-		else
+
+		return null;
+	}
+
+	@Override
+	public void getPath( final Uri uri, final PathResolverListener pathResolverListener )
+	{
+		StringRequest stringRequest = new StringRequest( uri.toString(), new Response.Listener<String>()
 		{
-			pathResolverListener.onPathError( App.getInstance().getString( R.string.could_not_resolve_url ) );
-		}
+			@Override
+			public void onResponse( String response )
+			{
+				Uri videoUrl = getVideoUrlFromResponse( response );
+
+				if ( videoUrl != null )
+				{
+					pathResolverListener.onPathResolved( videoUrl, null );
+				}
+				else
+				{
+					String mediaUrl = getMediaUrl( uri );
+
+					if ( mediaUrl != null )
+					{
+						pathResolverListener.onPathResolved( Uri.parse( mediaUrl + "?size=l" ), Uri.parse( mediaUrl + "?size=t" ) );
+					}
+					else
+					{
+						pathResolverListener.onPathError( App.getInstance().getString( R.string.could_not_resolve_url ) );
+					}
+				}
+			}
+		}, new Response.ErrorListener()
+		{
+			@Override
+			public void onErrorResponse( VolleyError error )
+			{
+				Log.v( TAG, error.toString() );
+
+				pathResolverListener.onPathError( error.toString() );
+			}
+		} );
+
+		RequestService.getInstance().addToRequestQueue( stringRequest );
 	}
 
 	@Override
