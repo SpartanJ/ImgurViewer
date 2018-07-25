e0e1ae80aac1098c682bf62d9df05c3ee503f769
Martín Lucas Golini <spartanj@gmail.com>
2018-07-24 22:56:26 -0300

    Swipe configuration.

--- app/src/main/java/com/ensoft/imgurviewer/view/activity/AlbumPagerActivity.java
+++ app/src/main/java/com/ensoft/imgurviewer/view/activity/AlbumPagerActivity.java
@@ -8,9 +8,17 @@ import android.support.annotation.Nullable;
 import android.support.v4.view.ViewPager;
 import android.widget.Toast;
 
+import com.ensoft.imgurviewer.App;
 import com.ensoft.imgurviewer.model.ImgurImage;
+import com.ensoft.imgurviewer.service.PreferencesService;
 import com.ensoft.imgurviewer.view.adapter.ImagesAlbumPagerAdapter;
+import com.ensoft.imgurviewer.view.fragment.ImageViewerFragment;
+import com.ensoft.imgurviewer.view.helper.SlidrPositionHelper;
 import com.imgurviewer.R;
+import com.r0adkll.slidr.Slidr;
+import com.r0adkll.slidr.model.SlidrConfig;
+import com.r0adkll.slidr.model.SlidrListener;
+import com.r0adkll.slidr.model.SlidrPosition;
 
 import java.util.Arrays;
 
@@ -28,6 +36,7 @@ public class AlbumPagerActivity extends AppActivity
 	}
 	
 	private ViewPager pager;
+	private ImagesAlbumPagerAdapter adapter;
 	
 	@Override
 	protected void onCreate( Bundle savedInstanceState )
@@ -37,6 +46,39 @@ public class AlbumPagerActivity extends AppActivity
 		setContentView( R.layout.activity_albumpager );
 		
 		pager = findViewById( R.id.view_pager );
+		
+		PreferencesService preferencesService = App.getInstance().getPreferencesService();
+		
+		SlidrPosition slidrPosition = SlidrPositionHelper.fromString( preferencesService.getGesturesImageView() );
+		
+		if ( preferencesService.gesturesEnabled() && ( slidrPosition == SlidrPosition.HORIZONTAL || slidrPosition == SlidrPosition.LEFT || slidrPosition == SlidrPosition.RIGHT ) )
+		{
+			Slidr.attach( this, new SlidrConfig.Builder().listener( new SlidrListener()
+			{
+				@Override
+				public void onSlideStateChanged( int state ) {}
+				
+				@Override
+				public void onSlideChange( float percent )
+				{
+					if ( null != adapter && null != adapter.getImageViewerFragment( pager.getCurrentItem() ) )
+					{
+						ImageViewerFragment fragment = adapter.getImageViewerFragment( pager.getCurrentItem() );
+						
+						if ( null != fragment.getContentContainer() )
+						{
+							fragment.getContentContainer().setBackgroundColor( (int) ( percent * 255.0f + 0.5f ) << 24 );
+						}
+					}
+				}
+				
+				@Override
+				public void onSlideOpened() {}
+				
+				@Override
+				public boolean onSlideClosed() { return false; }
+			} ).position( SlidrPositionHelper.fromString( preferencesService.getGesturesImageView() ) ).build() );
+		}
 	}
 	
 	@Override
@@ -56,7 +98,7 @@ public class AlbumPagerActivity extends AppActivity
 				
 				int initialPosition = bundle.getInt( PARAM_IMAGES_CUR_POSITION );
 				
-				pager.setAdapter( new ImagesAlbumPagerAdapter( getFragmentManager(), images ) );
+				pager.setAdapter( adapter = new ImagesAlbumPagerAdapter( getFragmentManager(), images ) );
 				pager.setCurrentItem( initialPosition );
 			}
 			catch ( Exception e )
