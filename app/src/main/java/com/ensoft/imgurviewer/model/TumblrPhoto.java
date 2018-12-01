6b201214c6cf5422568ff2e47d804e6098793116
Mart?n Lucas Golini <spartanj@gmail.com>
2018-12-01 03:25:32 -0300

    Fixed Tumblr solver.

--- app/src/main/java/com/ensoft/imgurviewer/model/TumblrPhoto.java
+++ app/src/main/java/com/ensoft/imgurviewer/model/TumblrPhoto.java
@@ -3,8 +3,13 @@ package com.ensoft.imgurviewer.model;
 import android.net.Uri;
 import android.text.TextUtils;
 
+import com.google.gson.annotations.SerializedName;
+
+import java.util.HashMap;
+
 public class TumblrPhoto
 {
+	@SerializedName( "mediaUrlTemplate" )
 	protected String url;
 	
 	protected String type;
@@ -12,6 +17,9 @@ public class TumblrPhoto
 	protected int width;
 	
 	protected int height;
+	
+	@SerializedName( "sizes" )
+	protected HashMap<String, TumblrSizes> sizes;
 	
 	public String getUrl()
 	{
@@ -33,8 +41,33 @@ public class TumblrPhoto
 		return height;
 	}
 	
+	protected TumblrSizes getBiggestSize()
+	{
+		TumblrSizes biggestSize = null;
+		
+		for ( TumblrSizes tumblrSize : sizes.values() )
+		{
+			if ( null == biggestSize || tumblrSize.width > biggestSize.width )
+			{
+				biggestSize = tumblrSize;
+			}
+		}
+		
+		return biggestSize;
+	}
+	
 	public Uri getUri()
 	{
+		if ( null != url )
+		{
+			TumblrSizes biggestSize = getBiggestSize();
+			
+			if ( null != biggestSize )
+			{
+				return Uri.parse( url.replace( "{id}", getBiggestSize().getId() ) );
+			}
+		}
+		
 		return !TextUtils.isEmpty( url ) ? Uri.parse( url ) : null;
 	}
 }
---
+++ app/src/main/java/com/ensoft/imgurviewer/model/TumblrSizes.java
@@ -0,0 +1,25 @@
+package com.ensoft.imgurviewer.model;
+
+public class TumblrSizes
+{
+	protected String id;
+	
+	protected int width;
+	
+	protected int height;
+	
+	public String getId()
+	{
+		return id;
+	}
+	
+	public int getWidth()
+	{
+		return width;
+	}
+	
+	public int getHeight()
+	{
+		return height;
+	}
+}
