dfea3a4074389d9cfc4b08f4f3721f10a9f598be
Mart?n Lucas Golini <spartanj@gmail.com>
2018-12-21 16:59:17 -0300

    Added share as media.

--- app/src/main/java/com/ensoft/imgurviewer/service/resource/FlickrService.java
+++ app/src/main/java/com/ensoft/imgurviewer/service/resource/FlickrService.java
@@ -7,10 +7,8 @@ import android.os.Looper;
 import android.text.TextUtils;
 import android.util.Log;
 
-import com.android.volley.NetworkResponse;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
-import com.android.volley.Response;
 import com.android.volley.toolbox.HttpStack;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
