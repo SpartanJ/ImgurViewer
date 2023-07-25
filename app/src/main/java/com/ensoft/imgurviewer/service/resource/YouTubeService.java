e51dc8a119fc2b6426c93bc1f2b6726127266a7d
sidit77 <sidit77@gmail.com>
2023-07-25 21:46:39 +0200

    support for loading youtube clip metadata

--- app/src/main/java/com/ensoft/imgurviewer/service/resource/YouTubeService.java
+++ app/src/main/java/com/ensoft/imgurviewer/service/resource/YouTubeService.java
@@ -22,6 +22,7 @@ import java.util.List;
 public class YouTubeService extends MediaServiceSolver  {
 
     private static final String PIPED_INSTANCE = "https://api.piped.yt";
+    private static final String YOUTUBE_CLIP_BASE_URL = "https://youtube.com/clip/";
 
     private PipedVideoStream findBestQuality(PipedVideoStream[] streams) {
         int bestHeight = -1;
@@ -51,7 +52,26 @@ public class YouTubeService extends MediaServiceSolver  {
             pathResolverListener.onPathError(uri, "Could not extract the video id");
             return;
         }
-        String endpoint = PIPED_INSTANCE + "/streams/" + info.id;
+        if(!info.clip) {
+            resolveVideo(uri, info.id, pathResolverListener);
+        } else {
+            RequestService.getInstance().makeStringRequest(Request.Method.GET, YOUTUBE_CLIP_BASE_URL + info.id, new ResponseListener<String>() {
+                @Override
+                public void onRequestSuccess(Context context, String response) {
+                    ClipInfo info = parseClipInfo(response);
+                    Log.i("YOUTUBE CLIP", "id: " + info.parent + " start: " + info.startMs + " end: " + info.endMs);
+                    //resolveVideo(uri, info.parent, pathResolverListener);
+                }
+                @Override
+                public void onRequestError(Context context, int errorCode, String errorMessage) {
+                    pathResolverListener.onPathError(uri, errorMessage);
+                }
+            });
+        }
+    }
+
+    private void resolveVideo(Uri uri, String id, PathResolverListener pathResolverListener) {
+        String endpoint = PIPED_INSTANCE + "/streams/" + id;
         RequestService.getInstance().makeJsonRequest(Request.Method.GET, endpoint, new ResponseListener<PipedStreamResponse>() {
             @Override
             public void onRequestSuccess(Context context, PipedStreamResponse response) {
@@ -63,15 +83,16 @@ public class YouTubeService extends MediaServiceSolver  {
                 pathResolverListener.onPathResolved(
                         Uri.parse(stream.getVideoUrl()),
                         MediaType.VIDEO_MP4,
-                        Uri.parse(response.getThumbnailUrl()) );
+                        uri );
             }
             @Override
             public void onRequestError(Context context, int errorCode, String errorMessage) {
                 pathResolverListener.onPathError(uri, errorMessage);
             }
         });
-
     }
+
+
 
     @Override
     public boolean isServicePath(Uri uri) {
@@ -98,7 +119,11 @@ public class YouTubeService extends MediaServiceSolver  {
             if(id != null)
                 return new VideoType(id, false);
             List<String> path = uri.getPathSegments();
-            if(path == null || path.size() < 2 || !path.get(0).matches("(embed|v|shorts)"))
+            if(path == null || path.size() < 2)
+                return null;
+            if(path.get(0).equals("clip"))
+                return new VideoType(path.get(1), true);
+            if(!path.get(0).matches("(embed|v|shorts)"))
                 return null;
             return new VideoType(path.get(1), false);
         }
@@ -114,6 +139,43 @@ public class YouTubeService extends MediaServiceSolver  {
             this.id = id;
             this.clip = clip;
         }
+    }
+
+    class ClipInfo {
+        public String parent;
+        public int startMs, endMs;
+
+        public ClipInfo(String parent, int startMs, int endMs) {
+            this.parent = parent;
+            this.startMs = startMs;
+            this.endMs = endMs;
+        }
+    }
+
+    private ClipInfo parseClipInfo(String response) {
+        String parent = extractKeyed(response, "\"videoId\"", '"', '"');
+        String clipConfig = extractKeyed(response, "\"clipConfig\"", '{', '}');
+        String start = extractKeyed(clipConfig, "\"startTimeMs\"", '"', '"');
+        String end = extractKeyed(clipConfig, "\"endTimeMs\"", '"', '"');
+        if(parent == null || start == null || end == null)
+            return null;
+
+        return new ClipInfo(parent, Integer.parseInt(start), Integer.parseInt(end));
+    }
+
+    private String extractKeyed(String haystack, String key, char startToken, char endToken) {
+        if(haystack == null)
+            return null;
+        int index = haystack.lastIndexOf(key);
+        if(index == -1)
+            return null;
+        int start = haystack.indexOf(startToken, index + key.length()) + 1;
+        if(start == 0)
+            return null;
+        int end = haystack.indexOf(endToken, start);
+        if(end == -1)
+            return null;
+        return haystack.substring(start, end);
     }
 
 }
