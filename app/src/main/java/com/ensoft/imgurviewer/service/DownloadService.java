f4cb9f85ac5fe0a4720f5e930e0e7fce461c092f
starry-shivam <starry@krsh.dev>
2024-05-03 23:25:47 +0530

    Fix formattig
    Signed-off-by: starry-shivam <starry@krsh.dev>

--- app/src/main/java/com/ensoft/imgurviewer/service/DownloadService.java
+++ app/src/main/java/com/ensoft/imgurviewer/service/DownloadService.java
@@ -7,29 +7,35 @@ import android.os.Environment;
 
 import java.io.File;
 
-public class DownloadService {
+public class DownloadService
+{
     private final Context context;
 
-    public DownloadService(Context context) {
+    public DownloadService( Context context )
+    {
         this.context = context;
     }
 
-    public void download(Uri uriDownload, String fileName) {
-        DownloadManager downloadManager = (DownloadManager) context.getSystemService(Context.DOWNLOAD_SERVICE);
+    public void download(Uri uriDownload, String fileName )
+    {
+        DownloadManager downloadManager = (DownloadManager) context.getSystemService( Context.DOWNLOAD_SERVICE );
 
-        DownloadManager.Request request = new DownloadManager.Request(uriDownload);
+        DownloadManager.Request request = new DownloadManager.Request( uriDownload );
 
-        request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+        request.setNotificationVisibility( DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED );
 
-        if (new PermissionService().isExternalStorageAccess(context)) {
+        if ( new PermissionService().isExternalStorageAccess( context ) )
+        {
             // Direct access to the Downloads folder is not allowed in Android 10 and above
             // when using scoped storage, so we need to use the subdirectory within download folder.
             String downloadSubDir = "ImgurViewer";
             request.setDestinationInExternalPublicDir(
                     Environment.DIRECTORY_DOWNLOADS, downloadSubDir + File.separator + fileName
             );
-        } else {
-            request.setDestinationInExternalFilesDir(context, Environment.DIRECTORY_DOWNLOADS, fileName);
+        }
+        else
+        {
+            request.setDestinationInExternalFilesDir( context, Environment.DIRECTORY_DOWNLOADS, fileName );
         }
 
         if (null != downloadManager) {
--- app/src/main/java/com/ensoft/imgurviewer/service/PermissionService.java
+++ app/src/main/java/com/ensoft/imgurviewer/service/PermissionService.java
@@ -5,33 +5,38 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.os.Build;
-
-import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
+import androidx.core.content.ContextCompat;
 
-public class PermissionService {
+public class PermissionService
+{
     public static final String READ_EXTERNAL_STORAGE = "android.permission.READ_EXTERNAL_STORAGE";
     public static int REQUEST_WRITE_EXTERNAL_STORAGE_PERMISSION = 0xF1;
     public static int REQUEST_READ_EXTERNAL_STORAGE_PERMISSION = 0xF2;
 
-    public boolean isExternalStorageAccess(Context context) {
+    public boolean isExternalStorageAccess( Context context )
+    {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
             // Scoped storage is used in Android 10 and above, so we don't
             // need to ask for permission to write to external storage when
             // using standard directories.
             return true;
         }
-        return ContextCompat.checkSelfPermission(context, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED;
+        return ContextCompat.checkSelfPermission( context, Manifest.permission.WRITE_EXTERNAL_STORAGE ) == PackageManager.PERMISSION_GRANTED;
     }
 
-    public boolean askExternalStorageAccess(Activity activity) {
-        if (!isExternalStorageAccess(activity)) {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+    public boolean askExternalStorageAccess( Activity activity )
+    {
+        if ( !isExternalStorageAccess( activity ) )
+        {
+            if ( Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q )
+            {
                 return false;
             }
 
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                activity.requestPermissions(new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_WRITE_EXTERNAL_STORAGE_PERMISSION);
+            if ( Build.VERSION.SDK_INT >= Build.VERSION_CODES.M )
+            {
+                activity.requestPermissions( new String[]{ android.Manifest.permission.WRITE_EXTERNAL_STORAGE }, REQUEST_WRITE_EXTERNAL_STORAGE_PERMISSION );
                 return true;
             }
         }
@@ -39,14 +44,18 @@ public class PermissionService {
         return false;
     }
 
-    public boolean askExternalStorageAccess(Fragment fragment) {
-        if (!isExternalStorageAccess(fragment.getActivity())) {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+    public boolean askExternalStorageAccess( Fragment fragment )
+    {
+        if ( !isExternalStorageAccess( fragment.getActivity() ) )
+        {
+            if ( Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q )
+            {
                 return false;
             }
 
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                fragment.requestPermissions(new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_WRITE_EXTERNAL_STORAGE_PERMISSION);
+            if ( Build.VERSION.SDK_INT >= Build.VERSION_CODES.M )
+            {
+                fragment.requestPermissions( new String[]{ android.Manifest.permission.WRITE_EXTERNAL_STORAGE }, REQUEST_WRITE_EXTERNAL_STORAGE_PERMISSION );
                 return true;
             }
         }
@@ -54,17 +63,22 @@ public class PermissionService {
         return false;
     }
 
-    public boolean isReadExternalStorageAccess(Context context) {
-        return ContextCompat.checkSelfPermission(context, READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED;
+    public boolean isReadExternalStorageAccess( Context context )
+    {
+        return ContextCompat.checkSelfPermission( context, READ_EXTERNAL_STORAGE ) == PackageManager.PERMISSION_GRANTED;
     }
 
-    public boolean askReadExternalStorageAccess(Fragment fragment) {
-        if (!isReadExternalStorageAccess(fragment.getActivity())) {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+    public boolean askReadExternalStorageAccess( Fragment fragment )
+    {
+        if ( !isReadExternalStorageAccess( fragment.getActivity() ) )
+        {
+            if ( Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q )
+            {
                 return false;
             }
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                fragment.requestPermissions(new String[]{READ_EXTERNAL_STORAGE}, REQUEST_READ_EXTERNAL_STORAGE_PERMISSION);
+            if ( Build.VERSION.SDK_INT >= Build.VERSION_CODES.M )
+            {
+                fragment.requestPermissions( new String[]{ READ_EXTERNAL_STORAGE }, REQUEST_READ_EXTERNAL_STORAGE_PERMISSION );
                 return true;
             }
         }
